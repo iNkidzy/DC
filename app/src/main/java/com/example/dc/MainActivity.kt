1e18f3c30e17144e26630910221513879721fcb4
Martin Emil Wøbbe <54886017+daiperboevs42@users.noreply.github.com>
2021-03-05 19:10:25 +0100

    changes to dice and overall commented out stuff

--- app/src/main/java/com/example/dc/MainActivity.kt
+++ app/src/main/java/com/example/dc/MainActivity.kt
@@ -4,78 +4,61 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.widget.ImageView
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 import java.util.*
-
+import android.widget.NumberPicker;
 
 class MainActivity : AppCompatActivity() {
     private val HISTORY_NAME ="history"
     private val TAG: String="xyz"
+    private val mRandomGenerator = Random()
+    private var numberOfDice: Int = 1;
+    private val diceImageId = intArrayOf(0, R.drawable.dice1,
+        R.drawable.dice2,
+        R.drawable.dice3,
+        R.drawable.dice4,
+        R.drawable.dice5,
+        R.drawable.dice6)
 
-
-    val diceIds= arrayOf(R.drawable.dice1, R.drawable.dice2, R.drawable.dice3,
-        R.drawable.dice4, R.drawable.dice5, R.drawable.dice6)
-    val mRandomGenerator = Random()
-    val mHistory = mutableListOf<Pair<Int,Int>>()
-
+    private val mHistory = mutableListOf<Pair<Int, Int>>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         btnRoll.setOnClickListener { v -> onRoll() }
-
-        imgDice1.setOnClickListener { v -> onRoll() }
-        imgDice2.setOnClickListener { v -> onRoll() }
-        btnClear.setOnClickListener { v -> onClear() }
+        NumberPicker.minValue = 1
+        NumberPicker.maxValue = 6
+        NumberPicker.setOnValueChangedListener{picker, oldVal,newVal -> numberOfDice = newVal; addDice()}
         Log.d(TAG, "OnCreate")
 
-        if(savedInstanceState != null)
-        {
-
-            val history = savedInstanceState.getSerializable(HISTORY_NAME) as Array<Pair<Int,Int>>
-            history.forEach{p->mHistory.add(p)}
-            updateHistory()
-            if(mHistory.size>0){
-                updateDiceWith(mHistory[mHistory.size-1])
-            }
-        }
     }
 
-
+    fun addDice(){
+        //first remove all dice
+        var dice = Array(numberOfDice) {i -> mRandomGenerator.nextInt(6) + 1}
+        dice.forEach {
+            var newView: ImageView
+            newView = ImageView(this)
+            //add new imageview to main page
+            //set dimensions of dice and somehow make them not collide
+            newView.setImageResource( diceImageId[it])}
+    }
 
     fun onRoll() {
-        val d1 = mRandomGenerator.nextInt(6)+1
-        val d2 = mRandomGenerator.nextInt(6)+1
-
-
-        imgDice1.setImageResource(diceIds[d1])
-        imgDice2.setImageResource(diceIds[d2])
-
-        mHistory.add(Pair(e1,e2))
-        if (mHistory.size > 5) mHistory.removeAt(0)
-        updateHistory()
-        Log.d(TAG, "Roll")
-
+        //check each die and roll it
     }
 
     private fun updateHistory() {
-        var s = ""
-        mHistory.forEach{p-> val e1= p.first; val e2= p.second
-                    s += "$e1-$e2\n"}
-         tvHistory.text = s
-
 
     }
     override fun onSaveInstanceState(outState: Bundle){
        super.onSaveInstanceState(outState)
         Log.d(TAG, "History saved")
-        outState.putSerializable(HISTORY_NAME, mHistory.toTypedArray())
 
     }
     fun onClear() {
-        mHistory.clear()
-        updateHistory()
     }
 
     fun onRoll(view: View) {
