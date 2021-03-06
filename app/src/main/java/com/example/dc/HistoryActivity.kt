d9d6aa2162ae29786efb31bfa17820b08685ed41
iNKidzy <nadq.nadia@gmail.com>
2021-03-07 00:01:11 +0100

    ListView

--- app/src/main/java/com/example/dc/HistoryActivity.kt
+++ app/src/main/java/com/example/dc/HistoryActivity.kt
@@ -13,15 +13,32 @@ import android.widget.ArrayAdapter
 import android.widget.BaseAdapter
 import android.widget.Button
 import android.widget.ListView
+import kotlin.collections.ArrayList
 
 class HistoryActivity : AppCompatActivity()  {
 
-    val translatedList = intent?.getStringArrayListExtra("diceList")
+    val translatedList = intent.getStringArrayListExtra("diceList")!!
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.second_activity)
         val listView = findViewById<ListView>(R.id.list)
+
+      val arrayList = ArrayList<String>()
+        arrayList.add("Dice 1")
+        arrayList.add("Dice 2")
+        arrayList.add("Dice 3")
+        arrayList.add("Dice 4")
+        arrayList.add("Dice 5")
+        arrayList.add("Dice 6")
+        arrayList.add("HEY")
+        arrayList.add("Buddy")
+        arrayList.add("We DID IT")
+
+        val arrayAdapter = ArrayAdapter(this,android.R.layout.simple_list_item_1,android.R.id.text1,translatedList)
+        listView.adapter = arrayAdapter
+
+
 
         val fab: View = findViewById(R.id.fabbtn2)
         fab.setOnClickListener {
